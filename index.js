7c412c9140e758fc0e1429035a68fb9b7fe23116
anordmann <anordmann@nomotech.com>
2024-01-29 10:57:28 +0100

    [UPD] Fetch to Axios + adding try catch + Async for public key save in a file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import jwkToPem from "jwk-to-pem";
 import dotEnv from 'dotenv'
 import * as fs from 'node:fs';
+import axios from 'axios';
 
 // Config .env file
 dotEnv.config()
@@ -16,58 +17,63 @@ async function getKeycloakConfig() {
 	if (keycloakConfig.wellKnown === '') {
 		console.error('No .env')
 		throw new Error("No .env");
-		return false;
 	}
 
 	// curl to get keycloakConfig
-	const response = await fetch(keycloakConfig.wellKnown, {
+	await axios({
+		url: keycloakConfig.wellKnown,
 		method: "GET", // *GET, POST, PUT, DELETE, etc.
 		headers: {
 			"Content-Type": "application/json",
 		}
-	});
-
-	keycloakConfig.wellKnownRes = await response.json();
+	}).then((response) => {
+		keycloakConfig.wellKnownRes = response;
+	}).catch((err) => console.log(err));
 
 	return keycloakConfig;
 }
 
 async function getJWKSig() {
-	let keycloakConfig = await  getKeycloakConfig()
+	let keycloakConfig = await getKeycloakConfig()
 	if (keycloakConfig === false) {
 		return false;
 	}
 
 	// curl to get JWK
-	const response = await fetch(keycloakConfig.wellKnownRes['jwks_uri'], {
+	let key = '';
+	await axios({
+		url: keycloakConfig.wellKnownRes.data.jwks_uri,
 		method: "GET", // *GET, POST, PUT, DELETE, etc.
 		headers: {
 			"Content-Type": "application/json",
 		}
-	});
-
-	let jwkSet = await response.json()
-	let key = '';
-	jwkSet.keys.forEach(jwk => {
-		if (jwk.alg === 'RS256') {
-			key = jwk
-		}
-	})
+	}).then((response) => {
+		response.data.keys.forEach(jwk => {
+			if (jwk.alg === 'RS256') {
+				key = jwk
+			}
+		})
+	}).catch((err) => console.log(err));
 
 	return key
 }
 
-function saveNewPEM(jwk) {
+async function saveNewPEM(jwk) {
 	// Get pem
 	let pem = jwkToPem(jwk);
 
 	////// NEED TO TEST IF THE JWK CORRECTLY DECODE JWT //////
 
 	// Save in public.pem
-	fs.writeFileSync('public.pem', pem);
+	try {
+		await fs.writeFileSync('public.pem', pem);
+	} catch (e) {
+		console.log('Error when file saved :');
+		console.log(e);
+	}
 	return pem
 }
 
 let jwk = await getJWKSig()
-let res = saveNewPEM(jwk)
-await console.log(res)
+let res = await saveNewPEM(jwk)
+console.log(res)
